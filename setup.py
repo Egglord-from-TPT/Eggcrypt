60ab8f600258f87fd0445d8dfdbd1821c52f6e0d
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-04 23:02:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="variables_plus",
-    version="3.1.0",
+    name="egg",
+    version="1.0.0",
     packages=find_packages(),
-    description="A simple library to create, clear, and delete global variables, safely.",
+    description="A pretty secure (yet inneficient) encryption program.",
     author="",
     python_requires=">=3.7",
 )
