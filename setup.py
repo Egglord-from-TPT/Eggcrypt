604b7d01ba22bb8ba285d0ab577b732527e16ca4
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-05 12:30:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="egg",
-    version="1.0.3",
+    version="1.1.0",
     packages=find_packages(),
     description="A pretty secure (yet inneficient) encryption program.",
     author="",
