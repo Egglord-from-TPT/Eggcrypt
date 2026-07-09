0486f145d123a59bcd0557a46918d708c272fa66
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-07-09 16:18:27 +0300

    Bump version from 1.4.0 to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="egg",
-    version="1.4.0",
+    version="2.0.0",
     packages=find_packages(),
     description="A pretty secure (yet inneficient) encryption program.",
     author="Egglord",
