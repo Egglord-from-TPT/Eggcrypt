5541adce60eb34aba90861d5c2a85d08cf889fa1
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-07-20 00:29:19 +0200

    Update egg.py

--- eggcrypt/egg.py
+++ eggcrypt/egg.py
@@ -1,3 +1,18 @@
+# eggcrypt - Copyright (C) 2026 Egglord-from-TPT
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from functools import lru_cache
 def replace_non_255(text):
     return ''.join(c if ord(c)<=255 else '?' for c in text)
